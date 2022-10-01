1b519ded28d4315d82cf99a47d7aaea50050374b
pawangupta5167 <52108857+pawangupta5167@users.noreply.github.com>
2022-10-01 15:29:54 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,12 @@
+# Python3 program to multiply all values in the
+# list using numpy.prod()
+
+import numpy
+list1 = [1, 2, 3]
+list2 = [3, 2, 4]
+
+# using numpy.prod() to get the multiplications
+result1 = numpy.prod(list1)
+result2 = numpy.prod(list2)
+print(result1)
+print(result2)
